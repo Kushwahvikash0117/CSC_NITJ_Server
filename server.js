eab9f02dc31a755ae93a018d224e9d5b3a24ad80
Vikash Kushwah <patelvikash0117@gmail.com>
2026-01-14 14:58:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import cors from "cors";
+
 import connectDB from "./config/db.js";
 
 import authRoutes from "./routes/authRoutes.js";
@@ -13,19 +15,25 @@ import { errorHandler } from "./middleware/errorMiddleware.js";
 connectDB();
 
 const app = express();
-
-// Middleware
 app.use(express.json());
 
-// Routes
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",       
+      "http://localhost:3000",
+    ],
+    credentials: true,
+  })
+);
+
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/blogs", blogRoutes);
-
-// Global error handler
 app.use(errorHandler);
-
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
